37a49ce32f8415c25d7e2dec93758de2230de788
Bryan Doerga <bryandoerga@gmail.com>
2017-08-03 20:10:28 +0200

    added insert item functionality

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
@@ -17,9 +17,18 @@ def categoryItems(category_id):
     items = session.query(Item).filter_by(category_id=category.id)
     return render_template('item.html', category=category, items=items)
 
-@app.route('/category/<int:category_id>/new/')
+
+@app.route('/category/<int:category_id>/new/', methods=['GET', 'POST'])
 def newItem(category_id):
-    return "page to create a new item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = Item(
+            name=request.form['name'], description=request.form['description'], category_id=category_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('categoryItems', category_id=category_id))
+    else:
+        return render_template('newitem.html', category_id=category_id)
+
 
 
 @app.route('/category/<int:category_id>/<int:item_id>/edit/')
