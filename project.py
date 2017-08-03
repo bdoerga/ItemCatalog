90fd35d89b25ac2e4213d45248f06ab97c6add1a
Bryan Doerga <bryandoerga@gmail.com>
2017-08-03 18:55:32 +0200

    added routes defs

--- project.py
+++ project.py
@@ -1,11 +1,43 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/categories/<int:category_id>/')
+def categoryItems(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id=category.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.description        
+        output += '</br>'
+        output += '</br>'
+    return output
+
+@app.route('/category/<int:category_id>/new/')
+def newItem(category_id):
+    return "page to create a new item. Task 1 complete!"
+
+
+@app.route('/category/<int:category_id>/<int:item_id>/edit/')
+def editItem(category_id, item_id):
+    return "page to edit a item. Task 2 complete!"
+
+
+@app.route('/category/<int:category_id>/<int:item_id>/delete/')
+def deleteItem(category_id, item_id):
+    return "page to delete a item. Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
