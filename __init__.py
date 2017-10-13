6155aea5f026317b1fa9e86657054c0754194546
Bryan Doerga <bryandoerga@gmail.com>
2017-10-13 20:43:57 +0200

    changed path to client key to absolute path from server

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ import requests
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+    open('/var/www/FlaskApp/FlaskApp/client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Restaurant Menu Application"
 
 
